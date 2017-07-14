72f8bd31ffd57139560b60bfb34e3d1c6ee35b3e
Devesh Rajput <Devesh.Rajput@phoenix.edu>
2017-07-14 10:52:00 +0530

    Test to get hold of restfb Jar

---
+++ service/src/main/java/com/fbgraph/api/test/Test.java
@@ -0,0 +1,44 @@
+package com.fbgraph.api.test;
+
+import static java.lang.System.out;
+
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.Version;
+import com.restfb.types.FriendList;
+import com.restfb.types.Page;
+import com.restfb.types.User;
+
+public class Test {
+
+	private static final String ACCESS_TOKEN = "asd";
+	private final FacebookClient fbClient;
+	public static void main(String []args){
+		
+	 	new Test().start();
+		
+		
+	}
+	
+	Test(){
+		fbClient = new DefaultFacebookClient(ACCESS_TOKEN,Version.VERSION_2_9);
+	}
+	
+	private void start(){
+		out.println("* Fetching Objects *");
+		
+		User user = fbClient.fetchObject("me", User.class);
+		out.println("User name: "+user.getName());
+		 getFriends(user);
+
+	}
+
+	private void getFriends(User user) {
+		out.println("* Fetching Friends");
+		Connection<User> myFriends = fbClient.fetchConnection("me/friends", User.class);
+		
+		out.println(myFriends.getTotalCount());
+	}
+	
+}
